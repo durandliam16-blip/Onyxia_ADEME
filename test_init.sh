e8822c00a12b95f530389060ff25322fef7f2c37
Durand <durand.liam16@gmail.com>
2025-10-27 16:41:36 +0100

    Update test_init.sh

--- test_init.sh
+++ test_init.sh
@@ -1,3 +1,5 @@
+echo 'lancement du télechargement'
+
 curl -L -o mssql-1.24.0.vsix https://github.com/microsoft/vscode-mssql/releases/download/v1.24.0/mssql-1.24.0.vsix 
 
 code-server --install-extension mssql-1.24.0.vsix
